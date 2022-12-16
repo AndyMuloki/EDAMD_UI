79a43a4feeac12958816b982d5467a6309decdd2
Andrew <andrew.muloki@strathmore.edu>
2022-12-16 18:49:59 +0300

    Make admin panel

--- cnn/views.py
+++ cnn/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Room
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -8,15 +9,22 @@ from django.shortcuts import render
 # action
 
 rooms = [
-    {'id':1, 'name':'Lets Learn python!'},
-    {'id':2, 'name':'Lets go!'},
-    {'id':3, 'name':'Lets Design!'},
+    {'id':1, 'name':'Get Diagnosed Now!'},
+    {'id':2, 'name':'Schedule an Appointment with us'},
+    
 ]
 
+
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'cnn/home.html', context)
 
 
-def room(request):
-    return render(request, 'cnn/room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'cnn/room.html', context)
